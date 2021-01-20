c2f547187a7ed6c4f5490c190ca4c1b686442cc5
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2021-01-20 17:27:39 +0800

    add example

--- webService/src/main/java/com/restful/sample/webService/user/UserController.java
+++ webService/src/main/java/com/restful/sample/webService/user/UserController.java
@@ -1,11 +1,15 @@
 package com.restful.sample.webService.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.util.List;
 
 @RestController
@@ -25,7 +29,8 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user){
-		return service.save(user);
+	public ResponseEntity<User> createUser(@RequestBody User user){
+		User newUser = service.save(user);
+		return new ResponseEntity<>(newUser,HttpStatus.CREATED);
 	}
 }
